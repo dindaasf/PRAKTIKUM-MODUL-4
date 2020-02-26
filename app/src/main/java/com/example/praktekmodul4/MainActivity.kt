cc63b38a71d000ab935eb89f5ff2c3eea681358e
unknown <dindongsf11@gmail.com>
2020-02-26 23:08:49 +0700

    praktikum 4

---
+++ app/src/main/java/com/example/praktekmodul4/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.praktekmodul4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val imgBtn = findViewById<ImageButton>(R.id.imgButtonAdd)
+        imgBtn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "berhasil", Toast.LENGTH_LONG).show()
+        }
+    }
+}
